67ba72fd1401cc74e3a80e15338a6cdce9314de5
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-02-15 21:48:27 +0530

    user model created ✅

---
+++ src/models/user.model.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
