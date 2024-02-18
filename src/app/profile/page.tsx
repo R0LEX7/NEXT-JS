c2bf1a5a15314654f57e3d31efce8ba35d2d7532
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-02-18 23:10:11 +0530

    imp profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import { Button } from "@mantine/core";
+import MyButton from "@/components/Custom/myButton";
+import { Profile } from "@/components/profile";
+import axios from "axios";
 
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  const [user, setUser] = useState(null);
 
-export default Page
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = async () => {
+    try {
+      const response = await axios.get("/api/users/profile");
+      console.log(response.data.user);
+      setUser(response.data.user);
+    } catch (error) {
+      console.log("error ", error);
+    }
+  };
+
+  return (
+    <div className=" w-full h-screen flex justify-center items-center">
+      {user && <Profile user={user} />}
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/profile.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Text, Paper, Title } from "@mantine/core";
+
+
+
+export function Profile({user}) {
+    console.log("props" , user);
+
+  return (
+    <Paper shadow="xs" radius="md" p="xl" className="bg-black  border border-[#AE3EC8]">
+      <Title  className="capitalize text-[#AE3EC8]" order={4}> Username : {user?.username}</Title>
+      <Text className="capitalize text-[#AE3EC8]" > email : {user?.email}</Text>
+      <Text></Text>
+    </Paper>
+  );
+}
